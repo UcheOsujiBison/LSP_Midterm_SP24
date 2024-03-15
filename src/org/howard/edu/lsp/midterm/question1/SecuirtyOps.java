c8e035dc8580d91bf67f9cb867d4f7d869aadd91
UcheOsujiBison <91709768+UcheOsujiBison@users.noreply.github.com>
2024-03-14 20:13:23 -0400

    Committed Question 1

---
+++ src/org/howard/edu/lsp/midterm/question1/SecuirtyOps.java
@@ -0,0 +1,38 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class SecuirtyOps {
+	/**
+	 * Encrypts a string with odd-index characters,
+	 * ignoring any punctuation and whitespace.
+	 * 
+	 * @param text the text to encrypt
+	 * @return The encrypted string
+	 * 
+	 * */
+	public static String encrypt(String text) {
+		if (text == null || text.isEmpty()) {
+			return "";
+		}
+		
+		//
+		StringBuilder encryptedText = new StringBuilder();
+		StringBuilder evenCharacters = new StringBuilder();
+		StringBuilder oddCharacters = new StringBuilder();
+		
+		// Separating the evenChars and oddChars
+		for (int i = 0; i < text.length(); i++) {
+			char ch = text.charAt(i);
+			if (Character.isLetterOrDigit(ch)) {
+				if (i % 2 == 0) {
+					evenCharacters.append(ch);
+				} else {
+					oddCharacters.append(ch);
+				}
+			}
+		}
+		
+		// Appending the evenChars followed by the oddChars
+		encryptedText.append(evenCharacters).append(oddCharacters);
+		return encryptedText.toString();
+	}
+}
